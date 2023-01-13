9bf91fcd5f2d5aca79e7247f70ab840b5cf2e110
OBrien-reece <indeche.evans.o@gmail.com>
2023-01-14 00:19:19 +0300

    Laravel push and stack implementation

---
+++ push_stack_test/resources/views/layouts/master.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+
+    {{--to define the title for pages--}}
+    <title>@yield('title')</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    @stack('styles')
+
+</head>
+<body>
+
+    <div id="app">
+        @yield('app')
+    </div>
+
+       @stack('scripts')
+
+</body>
+</html>
